84ec25d9f62727bedd61f311103e181d4bfdd8a6
Anoukeyx <146704478+Anoukeyx@users.noreply.github.com>
2023-11-06 11:33:27 +0100

    oush

--- js/E03-delete.js
+++ js/E03-delete.js
@@ -1,21 +1,43 @@
 import { supa } from "../supabase.js";
 
 const btn = document.querySelector('#loeschen');
-btn.addEventListener('click', deletePerson, deleteusers);
+btn.addEventListener('click', deleteFunktion);
 
-const Person = supabase.channel('custom-delete-channel')
-  .on(
-    'postgres_changes',
-    { event: 'DELETE', schema: 'public', table: 'Person' },
-    (payload) => {
-      console.log('Change received!', payload)
-    } 
+const deletePerson = email.value;
+const deleteusers = "uuid";
 
-    'postgres_changes',
-    { event: 'DELETE', schema: 'auth', table: 'users' },
-    (payload) => {
-      console.log('Change received!', payload)
-    } 
+const { error } = await supabase
+  .from('Person')
+  .delete()
+  .eq('email', email);
 
-  )
-  .subscribe()
+ deletePerson();
+
+ deleteusers();
+
+ deleteFunktion(deletePerson, deleteusers)
+
+
+
+
+
+async function deletePersonByEmail(email) {
+    try {
+        // Versuchen, die Person anhand ihrer E-Mail-Adresse aus der Datenbank zu löschen
+        const { data, error } = await supabase.from("Person").delete().eq("email", email);
+
+        if (error) {
+            // Bei einem Fehler die Fehlermeldung ausgeben
+            console.error("Fehler beim Löschen der Person:", error.message);
+        } else {
+            // Erfolgreich gelöscht
+            console.log("Person erfolgreich gelöscht.");
+        }
+    } catch (error) {
+        console.error("Ein unerwarteter Fehler ist aufgetreten:", error);
+    }
+}
+
+// Verwenden Sie die Funktion, um eine Person anhand ihrer E-Mail-Adresse zu löschen
+const emailToDelete = "beispiel@email.com"; // Hier ersetzen Sie "beispiel@email.com" durch die tatsächliche E-Mail-Adresse der zu löschenden Person
+deletePersonByEmail(emailToDelete);
