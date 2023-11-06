d79f9490e3add29c0cac4061d9d2070032d7d671
Anoukeyx <146704478+Anoukeyx@users.noreply.github.com>
2023-11-06 09:56:27 +0100

    nk

--- js/E03-delete.js
+++ js/E03-delete.js
@@ -1,3 +1,8 @@
+import { supa } from "../supabase.js";
+
+const btn = document.querySelector('#loeschen');
+btn.addEventListener('click', deletePerson, deleteusers);
+
 
 const Person = supabase.channel('custom-delete-channel')
   .on(
@@ -5,6 +10,13 @@ const Person = supabase.channel('custom-delete-channel')
     { event: 'DELETE', schema: 'public', table: 'Person' },
     (payload) => {
       console.log('Change received!', payload)
-    }
+    } 
+
+    'postgres_changes',
+    { event: 'DELETE', schema: 'auth', table: 'users' },
+    (payload) => {
+      console.log('Change received!', payload)
+    } 
+
   )
   .subscribe()
